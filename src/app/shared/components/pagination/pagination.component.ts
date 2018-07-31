cfa13ae758717ca4ac7c9a3180b82b91e8fb8719
Savasondja <artem.savenko@aily.team>
2018-07-31 13:02:59 +0300

    www

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {SearchParams} from '../../models/search.pagination.model';
 import {faChevronLeft, faChevronRight, faStepBackward, faStepForward} from '@fortawesome/free-solid-svg-icons';
 
